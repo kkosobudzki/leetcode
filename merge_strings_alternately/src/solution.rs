7a0f265a61a6d3657440bb4a0189b7b1e31d65a1
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-06-14 18:16:17 +0200

    feat(merge_strings_alternately): accepted solution

--- merge_strings_alternately/src/solution.rs
+++ merge_strings_alternately/src/solution.rs
@@ -1,5 +1,22 @@
 pub fn merge_alternately(word1: String, word2: String) -> String {
-    "".into()
+    let mut chars1 = word1.chars();
+    let mut chars2 = word2.chars();
+
+    let mut result: String = "".to_owned();
+
+    loop {
+        match (chars1.next(), chars2.next()) {
+            (Some(a), Some(b)) => {
+                result.push(a);
+                result.push(b);
+            }
+            (Some(a), None) => result.push(a),
+            (None, Some(b)) => result.push(b),
+            (None, None) => break,
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
