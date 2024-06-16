f41aa74ee7a772663f7b7bf8fec5ee22c896a0d2
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-06-16 19:16:13 +0200

    feat(reverse_linked_list): accepted solution

---
+++ reverse_linked_list/src/solution.rs
@@ -0,0 +1,57 @@
+use std::collections::VecDeque;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut current: Option<Box<ListNode>> = head;
+
+    let mut queue = VecDeque::new();
+
+    loop {
+        match current {
+            Some(boxed) => {
+                queue.push_front(boxed.val);
+
+                current = boxed.next;
+            }
+            None => break,
+        }
+    }
+
+    queue.iter().fold(None, |next, &value| {
+        let mut node = ListNode::new(value);
+        node.next = next;
+
+        Some(Box::new(node))
+    })
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::ListNode;
+
+//     #[test]
+//     fn ex1() {
+//         let mut next = ListNode::new(5);
+
+//         for i in 4..1 {
+//             let mut node = ListNode::new(i);
+//             node.link(next);
+
+//             next = node;
+//         }
+
+//         let
+//     }
+// }
