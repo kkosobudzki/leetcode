d5008bb9834858433f46a5455d4890d76d94d88c
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-06-25 13:46:13 +0200

    chore: clean up

--- longest_zigzag_in_a_binary_tree/src/solution.rs
+++ longest_zigzag_in_a_binary_tree/src/solution.rs
@@ -1,7 +1,5 @@
 use std::cell::RefCell;
 use std::cmp;
-use std::collections::VecDeque;
-use std::iter::once;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -49,8 +47,6 @@ fn length(root: &Option<Rc<RefCell<TreeNode>>>, is_left: bool, depth: i32) -> i3
         Some(rc) => {
             let node = rc.borrow();
 
-            // println!("length, node: {:?}, direction: {:?}", node, direction);
-
             if is_left {
                 cmp::max(
                     length(&node.left, false, depth + 1),
@@ -64,65 +60,5 @@ fn length(root: &Option<Rc<RefCell<TreeNode>>>, is_left: bool, depth: i32) -> i3
             }
         }
         None => depth,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{longest_zig_zag, TreeNode};
-
-    // #[test]
-    // fn ex1() {
-    //     let tree = vec![
-    //         Some(1),
-    //         None,
-    //         Some(2),
-    //         None,
-    //         None,
-    //         Some(3),
-    //         Some(4),
-    //         Some(5),
-    //         Some(6),
-    //         None,
-    //         Some(7),
-    //         None,
-    //         None,
-    //         None,
-    //         Some(8),
-    //     ];
-    //
-    //     let result = longest_zig_zag(TreeNode::from_vec(tree));
-    //
-    //     assert_eq!(result, 3);
-    // }
-
-    #[test]
-    fn ex2() {
-        let tree = vec![
-            Some(1),
-            Some(2),
-            Some(3),
-            None,
-            Some(4),
-            None,
-            None,
-            Some(5),
-            Some(6),
-            None,
-            Some(7),
-        ];
-
-        let result = longest_zig_zag(TreeNode::from_vec(tree));
-
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn ex3() {
-        let tree = vec![Some(1)];
-
-        let result = longest_zig_zag(TreeNode::from_vec(tree));
-
-        assert_eq!(result, 0);
     }
 }
